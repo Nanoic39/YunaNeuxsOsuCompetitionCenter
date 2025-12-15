cf6937ef58fbd403b53b3501ca73d5011cd7b0b9
Nanoic <2026256242@qq.com>
2025-12-15 18:40:35 +0800

    first commit

---
+++ server/src/main/java/cc/nanoic/yuna/osu/YunaNexusOsuCompetitionCenterServerApplication.java
@@ -0,0 +1,13 @@
+package cc.nanoic.yuna.osu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunaNexusOsuCompetitionCenterServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunaNexusOsuCompetitionCenterServerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/cc/nanoic/yuna/osu/YunaNexusOsuCompetitionCenterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.nanoic.yuna.osu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunaNexusOsuCompetitionCenterServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
